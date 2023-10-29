6f7b72295196c9031f4cf3d11d79b5c1520b0e2a
Elias Ablan <eliasablan93@gmail.com>
2023-10-29 03:12:14 -0400

    section header svg detail fixed

--- components/Contact.js
+++ components/Contact.js
@@ -21,44 +21,42 @@ function Contact() {
                 <h2 className="text-2xl leading-normal mb-2 font-bold text-gray-800">
                   <span className="font-light">Contact</span> Us
                 </h2>
-                {/* <svg
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 100 60"
-                    style="margin: 0 auto; height: 35px"
-                    xml:space="preserve"
-                  >
-                    <circle
-                      cx="50.1"
-                      cy="30.4"
-                      r="5"
-                      className="stroke-primary"
-                      style="
-                        fill: transparent;
-                        stroke-width: 2;
-                        stroke-miterlimit: 10;
-                      "
-                    ></circle>
-                    <line
-                      x1="55.1"
-                      y1="30.4"
-                      x2="100"
-                      y2="30.4"
-                      className="stroke-primary"
-                      style="stroke-width: 2; stroke-miterlimit: 10"
-                    ></line>
-                    <line
-                      x1="45.1"
-                      y1="30.4"
-                      x2="0"
-                      y2="30.4"
-                      className="stroke-primary"
-                      style="stroke-width: 2; stroke-miterlimit: 10"
-                    ></line>
-                  </svg> */}
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 100 60"
+                  style={{ margin: "0 auto", height: "35px" }}
+                >
+                  <circle
+                    cx="50.1"
+                    cy="30.4"
+                    r="5"
+                    className="stroke-primary"
+                    style={{
+                      fill: "transparent",
+                      strokeWidth: "2",
+                      strokeMiterlimit: "10",
+                    }}
+                  ></circle>
+                  <line
+                    x1="55.1"
+                    y1="30.4"
+                    x2="100"
+                    y2="30.4"
+                    className="stroke-primary"
+                    style={{ strokeWidth: 2, strokeMiterlimit: 10 }}
+                  ></line>
+                  <line
+                    x1="45.1"
+                    y1="30.4"
+                    x2="0"
+                    y2="30.4"
+                    className="stroke-primary"
+                    style={{ strokeWidth: 2, strokeMiterlimit: 10 }}
+                  ></line>
+                </svg>
                 <p className="text-gray-600 leading-relaxed font-light text-xl mx-auto pb-2">
                   Have questions about service, please contact us.
                 </p>
