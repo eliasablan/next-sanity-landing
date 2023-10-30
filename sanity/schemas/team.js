7010a37b3a643b2bcf4c8e082f4dccffd55cf332
Elias Ablan <eliasablan93@gmail.com>
2023-10-30 05:33:57 -0400

    sanity team image added

--- sanity/schemas/team.js
+++ sanity/schemas/team.js
@@ -6,6 +6,21 @@ export default defineType({
   title: "Equipo",
   fields: [
     {
+      name: "image",
+      type: "image",
+      title: "Imagen",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alt",
+        },
+      ],
+    },
+    {
       name: "name",
       type: "string",
       title: "Nombre",
