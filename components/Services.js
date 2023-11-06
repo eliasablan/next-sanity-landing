dcfaaf88c2aa290562048aa9ff53aac449a941dd
Elias Ablan <eliasablan93@gmail.com>
2023-11-05 22:48:18 -0400

    DynamicIconRenderer applied to Services component

--- components/Services.js
+++ components/Services.js
@@ -1,17 +1,18 @@
 import { getEverything } from "@/sanity/sanity-utils";
-import * as FaIcons from "react-icons/fa"; // Font Awesome Icons
-import * as SIcons from "react-icons/si"; // Simple Icons
-import * as FIcons from "react-icons/fi"; // Feather Icons
-import * as HIcons from "react-icons/hi"; // Hero Icons
-import * as MdIcons from "react-icons/md"; // Material Design Icons
+import DynamicIconRenderer from "./DynamicIconRenderer";
+// import * as FaIcons from "react-icons/fa"; // Font Awesome Icons
+// import * as SIcons from "react-icons/si"; // Simple Icons
+// import * as FIcons from "react-icons/fi"; // Feather Icons
+// import * as HIcons from "react-icons/hi"; // Hero Icons
+// import * as MdIcons from "react-icons/md"; // Material Design Icons
 
-const DynamicIconRenderer = ({ name, provider }) => {
-  if (provider == "fa") return FaIcons[name]; // Font Awesome Icons
-  if (provider == "si") return SIcons[name]; // Simple Icons
-  if (provider == "fi") return FIcons[name]; // Feather Icons
-  if (provider == "hi") return HIcons[name]; // Hero Icons
-  if (provider == "mdi") return MdIcons[name]; // Material Design Icons
-};
+// const DynamicIconRenderer = ({ name, provider }) => {
+//   if (provider == "fa") return FaIcons[name]; // Font Awesome Icons
+//   if (provider == "si") return SIcons[name]; // Simple Icons
+//   if (provider == "fi") return FIcons[name]; // Feather Icons
+//   if (provider == "hi") return HIcons[name]; // Hero Icons
+//   if (provider == "mdi") return MdIcons[name]; // Material Design Icons
+// };
 
 const Services = async () => {
   const everything = await getEverything();
@@ -73,28 +74,28 @@ const Services = async () => {
         {/* row */}
         <div className="flex flex-wrap flex-row -mx-4 text-center">
           {services
-            ? services.map((service) => {
-                let Icon = DynamicIconRenderer(service.icon);
-                return (
-                  <div
-                    key={service._id}
-                    className="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 fade-up-show"
-                  >
-                    {/* service block */}
-                    <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
-                      <div className="inline-block text-gray-700 mb-2">
-                        {/* icon */}
-                        <Icon className="h-11 w-11" />
-                      </div>
-                      <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
-                        {service.title}
-                      </h3>
-                      <p className="text-gray-500">{service.description}</p>
+            ? services.map((service) => (
+                <div
+                  key={service._id}
+                  className="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 fade-up-show"
+                >
+                  {/* service block */}
+                  <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+                    <div className="inline-block text-gray-700 mb-2">
+                      {/* icon */}
+                      <DynamicIconRenderer
+                        icon={service.icon}
+                        className="h-11 w-11"
+                      />
                     </div>
-                    {/* end service block */}
+                    <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
+                      {service.title}
+                    </h3>
+                    <p className="text-gray-500">{service.description}</p>
                   </div>
-                );
-              })
+                  {/* end service block */}
+                </div>
+              ))
             : ""}
         </div>
         {/* end row */}
